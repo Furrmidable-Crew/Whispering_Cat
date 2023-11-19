8998f53a4e8825b6c042648a0d57c81d5e9841a2
Pingdred <ema.morrone01.em@gmail.com>
2023-11-19 17:52:46 +0100

    file renamed

---
+++ audio_parser.py
@@ -0,0 +1,36 @@
+from typing import Iterator
+from langchain.document_loaders.base import BaseBlobParser
+from langchain.document_loaders.blob_loaders import Blob
+from langchain.schema import Document
+import requests
+
+def transcript(key, lang, file):
+    # TODO: Check if the file is too large for OpenAI (max 25mb)
+    res = requests.post(
+                    "https://api.openai.com/v1/audio/transcriptions", 
+                    headers={
+                        "Authorization": f"Bearer {key}"
+                    }, files={
+                        "file": file
+                    },
+                    data= {
+                        "model": "whisper-1",
+                        "language": lang
+                    }
+                )
+    json = res.json()
+    return json['text']
+
+class AudioParser(BaseBlobParser):
+    """Parser for audio blobs."""
+
+    def __init__(self, key: str, lang: str = "en"):
+        self.key = key
+        self.lang = lang
+
+    def lazy_parse(self, blob: Blob) -> Iterator[Document]:
+        """Lazily parse the blob."""
+
+        content = transcript(self.key, self.lang, (blob.path, blob.as_bytes(), blob.mimetype))
+
+        yield Document(page_content=content, metadata={"source": "whispering_cat", "name": blob.path.rsplit('.', 1)[0]})
