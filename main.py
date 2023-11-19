059bad0081bc0fe46cfd6e8c7cbd2e6c68940464
Pingdred <ema.morrone01.em@gmail.com>
2023-11-19 18:09:00 +0100

    Added file handler for mime type audio/x-wav

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ def rabbithole_instantiates_parsers(file_handlers: dict, cat) -> dict:
     new_file_handlers["audio/mpeg"] = AudioParser(settings["api_key"], settings["language"])
     new_file_handlers["audio/webm"] = AudioParser(settings["api_key"], settings["language"])
     new_file_handlers["audio/wav"] = AudioParser(settings["api_key"], settings["language"])
+    new_file_handlers["audio/x-wav"] = AudioParser(settings["api_key"], settings["language"])
     new_file_handlers["audio/ogg"] = AudioParser(settings["api_key"], settings["language"])
     new_file_handlers["video/mp4"] = AudioParser(settings["api_key"], settings["language"])
 
